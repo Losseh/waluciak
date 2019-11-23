4872499dc6a4d598c266c127efe707deb0e4f75d
Losseh <aszymanski90@gmail.com>
2019-11-24 00:12:56 +0100

    update

--- gold.py
+++ gold.py
@@ -29,19 +29,22 @@ def compute(params, current_gold_price):
 current_gold_price = get_current_gold_price()
 
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-weight', type=float, help='weight of coin')
-parser.add_argument('-fineness', type=int, help='fineness of coin')
+parser.add_argument('-fineness', type=float, help='fineness of coin')
 parser.add_argument('-price', type=float, help='required price')
 
 args = parser.parse_args()
 
+assert args.weight is not None, "weight argument is required!"
+assert args.fineness is not None, "fineness argument is required!"
+assert args.price is not None, "price argument is required!"
 
 params = {
     'weight': args.weight,
     'fineness': args.fineness,
     'price': args.price
 }
+print params
 
 print compute(params, current_gold_price)
